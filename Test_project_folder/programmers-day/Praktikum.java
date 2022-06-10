2c0628e9407da4a6b6836766fed28e251e8bc583
Tristar <sergstar777@mail.ru>
2022-06-10 09:10:37 +0300

    выполнено

--- Test_project_folder/programmers-day/Praktikum.java
+++ Test_project_folder/programmers-day/Praktikum.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Praktikum {
-}
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            // здесь нужно вывести результат
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        // здесь нужно определить, является ли переданный год високосным
+    }
+} 
